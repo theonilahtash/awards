04612f0d888b90c15c0d63dd687a4822738950b4
Theonilah Owali <theonilahtash@gmail.com>
2018-12-20 07:42:49 +0300

    added a profile class and its instances

--- award/models.py
+++ award/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    bio = models.CharField(max_length=100,null=True)
+    profile_pic = models.ImageField(upload_to='profile/')
+
+    def __str__(self):
+        return self.username
+
+
