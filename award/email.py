5577207da81b1e2fa8fc983dbe02aa39e522887a
Theonilah Owali <theonilahtash@gmail.com>
2018-12-21 20:58:24 +0300

    added a django mail module that is responsible for sending emails

--- award/email.py
+++ award/email.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to the award zone'
-    sender = 'james@moringaschool.com'
+    sender = 'theonilahtash@gmail.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/awardemail.txt',{"name": name})
