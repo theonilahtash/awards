057d007a98fc519365286640742d262f1eee7883
Theonilah Owali <theonilahtash@gmail.com>
2018-12-21 16:03:32 +0300

    defined profile and search letter

--- award/models.py
+++ award/models.py
@@ -7,17 +7,30 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     bio = models.CharField(max_length=100,null=True)
     profile_pic = models.ImageField(upload_to='profile/')
+    phone = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
 
 class Project(models.Model):
     posted_by = models.ForeignKey(User, null=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
-    name = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, null=True)
     project_image = models.ImageField(upload_to='projects/',null=True)
     description = models.TextField(null=True)
     project_link = models.TextField(null=True)
 
 
+    def get_projects(cls):
+        projects = Project.objects.all()
+        return projects
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title_icontains=search_term)
+        return projects
+
+class AwardLetterReciepients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
--- award/views.py
+++ award/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from .models import Profile,Project,AwardLetterReciepients
+from .email import send_welcome_email
+from .forms import AwardLetterForm
+
 
 
 # Create your views here.
 def welcome(request):
-    # award = projects.objects.all()
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    if request.method =='POST':
+        form = AwardLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = AwardLetterRecipients(name = name,email = email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('welcome.html')
+            print('valid')
+    else:
+        form = AwardLetterForm()
+
+    return render(request, 'index.html',{"projects":projects,"letterForm":form})
 
 
+def profile(request,profile_id):
+    try:
+        profile = Profile.objects.get(id = profile_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-award/profile.html",{"profile":profile})
+
+    
+   
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-award/search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "Searched"
+        return render(request, 'all-award/search.html',{"message":message})
