504568e43fc822efd4f0c1f2ab2409e2ad4c9c81
Theonilah Owali <theonilahtash@gmail.com>
2018-12-24 12:02:32 +0300

    added a login form for already registered users and a place for sign up

--- zone/urls.py
+++ zone/urls.py
@@ -22,7 +22,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('award.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
+ 
 
 
 ]
